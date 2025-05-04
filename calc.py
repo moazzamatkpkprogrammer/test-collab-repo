d28574f65246c53af12863f940994ecde4a70561
moazzamatkpkprogrammer <moazzam.ali.work@gmail.com>
2025-05-04 11:07:54 +0500

    Age related conditions are added.

--- calc.py
+++ calc.py
@@ -1,2 +1,8 @@
 name = input("What is your name?")
 print(f"Hello, {name}")
+#More changes by user 1 
+age = int(input("What is your age?"))
+if age<18:
+    print(f"{name} not allowed")
+else:
+    print(f"{name} allowed")
